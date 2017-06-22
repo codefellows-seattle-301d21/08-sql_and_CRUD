4d571a48b705e618976e65ca3a195a8a5deb416f
DKosmos <Davidakosmos@gmail.com>
2017-06-21 17:31:50 -0700

    adds my username and password to test page, then removes my username and password for security reasons

--- starter-code/server.js
+++ starter-code/server.js
@@ -13,8 +13,8 @@ const app = express();
 // TODO: Complete the connection string for the url that will connect to your local postgres database
 // Windows and Linux users; You should have retained the user/pw from the pre-work for this course.
 // Your url may require that it's composed of additional information including user and password
-// const conString = 'postgres://USER:PASSWORD@HOST:PORT/DBNAME';
-const conString = 'postgres://localhost:5432';
+// const conString = 'postgres://USER:PASSWORD@HOST:PORT/DBNAME';  Need to use my username and password, but I'm not uploading that to github.
+const conString = 'postgres://@localhost:5432/demo';
 
 // TODO: Our pg module has a Client constructor that accepts one argument: the conString we just defined.
 //       This is how it knows the URL and, for Windows and Linux users, our username and password for our
